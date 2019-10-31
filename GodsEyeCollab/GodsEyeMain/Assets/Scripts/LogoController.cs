28f47032f17db7009f4c905edb36cf50a0db3b03
v-zamarian <zamarian@usc.edu>
2019-10-31 13:01:59 -0700

    Company logo is pulled from the correct directory.

--- GodsEyeCollab/GodsEyeMain/Assets/Scripts/LogoController.cs
+++ GodsEyeCollab/GodsEyeMain/Assets/Scripts/LogoController.cs
@@ -9,7 +9,7 @@ public class LogoController : MonoBehaviour
     public GameObject companyLogo;
 
     //target's company logo will be stored in this directory when pulling in user data
-    public string recentImageDir = "Testing/EmployerLogo";
+    public string recentImageDir = "gagan_company";
 
     Sprite logo;
 
@@ -20,13 +20,8 @@ public class LogoController : MonoBehaviour
     }
 
     void ImportCompanyLogo(){
-        Object[] images = Resources.LoadAll(recentImageDir, typeof(Texture2D));
-
-        //for all images in the directory (there should be only one), create a sprite for them
-        foreach (Object obj in images){
-            Texture2D tex = (Texture2D)obj;
-            logo = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
-        }
+        Texture2D tex = Resources.LoadAll(recentImageDir)[0] as Texture2D;
+        logo = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
 
         companyLogo.GetComponentInChildren<Image>().sprite = logo;
         companyLogo.GetComponentInChildren<Image>().preserveAspect = true;
